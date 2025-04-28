51c688c61ce7ad963bef928a47f2d1e081c28bbb
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-28 14:43:10 +0700

    Add - Form dan text form field beserta button

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ucp1/presentation/login_page.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,8 +9,106 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController namalengkapController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nohpController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController konfirpasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  
+
+  bool _isPasswordVisible = false;
+  bool _isPasswordKonfirmVisible = false;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _isPasswordVisible = false;
+    _isPasswordKonfirmVisible =  false;
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: const AssetImage("assets/images/hd.png"),
+                height: size.height * 0.1,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'SELAMAT DATANG KEMBALI',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Nama Lengkap',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.email),
+                      hintText: 'Nama Lengkap',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nama Lengkap Tidak Boleh Kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Daftar',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
