7475d856fa5c2a7cb566e04902c5e6f72aa79590
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-29 18:18:54 +0700

    Update - Penyesuaian terhadap UI

--- lib/presentation/detail_piket_page.dart
+++ lib/presentation/detail_piket_page.dart
@@ -22,24 +22,67 @@ class DetailPiketPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail $tugas'),
+        title: Text('Detail $tugas',
+        style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
         backgroundColor: Colors.deepOrange,
+        iconTheme: const IconThemeData(
+    color: Colors.white, // <- ini yang mengatur warna ikon back
+  ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              formatTanggalDenganHari(tanggal),
-              style: const TextStyle(
-                fontSize: 18,
-                color: Colors.deepOrange,
-                fontWeight: FontWeight.w500,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  formatTanggalDenganHari(tanggal),
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.deepOrange,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 6,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    nama,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            Card(
+              color: Colors.deepOrange,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Center(
+                  child: Text(
+                    tugas,
+                    style: const TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
               ),
             ),
-          ]
+          ],
         ),
+      ),
     );
   }
 }
