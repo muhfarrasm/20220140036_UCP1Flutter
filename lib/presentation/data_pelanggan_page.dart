70836aaff64fedaf22351d3e5bbd7247849bda0a
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-29 19:11:56 +0700

    Add - membuat page data pelanggan

---
+++ lib/presentation/data_pelanggan_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class DataPelangganPage extends StatefulWidget {
+  const DataPelangganPage({super.key});
+
+  @override
+  State<DataPelangganPage> createState() => _DataPelangganPageState();
+}
+
+class _DataPelangganPageState extends State<DataPelangganPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController hpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController posController = TextEditingController();
+
+
+void _resetForm() {
+    _formKey.currentState!.reset();
+    namaController.clear();
+    emailController.clear();
+    hpController.clear();
+    alamatController.clear();
+    provinsiController.clear();
+    posController.clear();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDF5F2),
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: const Text(
+          'Data Pelanggan',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              
+            ),
+        )
+         ),
+
+    );
+  }
+}
