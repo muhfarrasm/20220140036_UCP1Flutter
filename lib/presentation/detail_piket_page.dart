fe401bec593e723c6bb23f34e17518547be12d7c
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-29 17:55:33 +0700

    Add- AppBar

--- lib/presentation/detail_piket_page.dart
+++ lib/presentation/detail_piket_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DetailPiketPage extends StatelessWidget {
   final String tugas;
@@ -12,14 +13,33 @@ class DetailPiketPage extends StatelessWidget {
     required this.tanggal,
   });
 
+  String formatTanggalDenganHari(DateTime date) {
+    final formatter = DateFormat('EEEE, dd MMMM yyyy', 'id_ID');
+    return formatter.format(date);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detail Tugas Piket'),
+        title: Text('Detail $tugas'),
         backgroundColor: Colors.deepOrange,
       ),
-      body: Center(child: Text(tugas, style: const TextStyle(fontSize: 24))),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              formatTanggalDenganHari(tanggal),
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.deepOrange,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ]
+        ),
     );
   }
 }
