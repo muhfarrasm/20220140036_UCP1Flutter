ed17e3e7daa2cb9485488d1d44604599cc187bcc
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-29 21:38:38 +0700

    Add - Button selesai dan kode pos

--- lib/presentation/data_detail_pelanggan.dart
+++ lib/presentation/data_detail_pelanggan.dart
@@ -8,15 +8,15 @@ class DataDetailPelanggan extends StatelessWidget {
   final String provinsi;
   final String kodePos;
 
-  const DataDetailPelanggan({super.key,
-  
-   required this.nama,
+  const DataDetailPelanggan({
+    super.key,
+
+    required this.nama,
     required this.email,
     required this.hp,
     required this.alamat,
     required this.provinsi,
     required this.kodePos,
-  
   });
 
   @override
@@ -27,12 +27,15 @@ class DataDetailPelanggan extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         foregroundColor: Colors.white,
         title: Text('Detail $nama'),
-       centerTitle: true,
-       titleSpacing: 0,
+        centerTitle: true,
+        titleSpacing: 0,
       ),
-       body: Padding(
-        padding: const EdgeInsets.all(20), // Padding untuk jarak konten dengan tepi layar
-        child: SingleChildScrollView( // Agar konten bisa di-scroll jika melebihi layar
+      body: Padding(
+        padding: const EdgeInsets.all(
+          20,
+        ), // Padding untuk jarak konten dengan tepi layar
+        child: SingleChildScrollView(
+          // Agar konten bisa di-scroll jika melebihi layar
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -45,26 +48,66 @@ class DataDetailPelanggan extends StatelessWidget {
                 const SizedBox(height: 16),
                 Text(
                   nama,
-                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 const SizedBox(height: 4),
-                Text(
-                  email,
-                  style: const TextStyle(fontSize: 16),
-                ),
+                Text(email, style: const TextStyle(fontSize: 16)),
                 const SizedBox(height: 2),
-                Text(
-                  hp,
-                  style: const TextStyle(fontSize: 16),
-                ),
+                Text(hp, style: const TextStyle(fontSize: 16)),
                 const SizedBox(height: 16),
-                
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildLabel("Provinsi"),
+                          buildTextField(provinsi),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildLabel("Kode Pos"),
+                          buildTextField(kodePos),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Selesai",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
-
-       ),
+      ),
     );
   }
+  
 }
