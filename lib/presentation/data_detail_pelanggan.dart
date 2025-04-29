d1c6d1d3afcb01a264deed5e3b114cf325d4dfcb
muhfarrasm <muhammadfarrasm@gmail.com>
2025-04-29 21:32:05 +0700

    Add- Text dan profile

--- lib/presentation/data_detail_pelanggan.dart
+++ lib/presentation/data_detail_pelanggan.dart
@@ -22,26 +22,49 @@ class DataDetailPelanggan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrange,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.deepOrange,
         foregroundColor: Colors.white,
         title: Text('Detail $nama'),
+       centerTitle: true,
+       titleSpacing: 0,
       ),
-      body: Padding(padding: const EdgeInsets.all(20),
-      child: Column(
-        children: [
-          const CircleAvatar(
-            radius: 60,
-            backgroundImage: AssetImage(
-              'assets/images/cowo.png'
+       body: Padding(
+        padding: const EdgeInsets.all(20), // Padding untuk jarak konten dengan tepi layar
+        child: SingleChildScrollView( // Agar konten bisa di-scroll jika melebihi layar
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/cowo.png'),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  nama,
+                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  email,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 2),
+                Text(
+                  hp,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+                
+              ],
             ),
           ),
-        ],
-      ),
-      )
+        ),
 
-
+       ),
     );
   }
 }
